25432c9c7d1a07b4d13a2998ba472ebb4b6bc673
ibrahim muhammad <ibrex29@gmail.com>
2024-07-17 16:41:13 +0100

    datetime

--- src/modules/editor/dtos/assign-reviewer.dto.ts
+++ src/modules/editor/dtos/assign-reviewer.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsISO8601, IsNotEmpty, IsUUID } from 'class-validator';
+import { isDate, IsDateString, IsISO8601, IsNotEmpty, IsOptional, IsUUID } from 'class-validator';
 
 export class AssignReviewerDto {
   @ApiProperty({
@@ -13,14 +13,16 @@ export class AssignReviewerDto {
     description: 'The unique ID of the reviewer who will be assigned to the manuscript',
     example: 'b1b8c0b5-3d2b-4b98-91e2-df27b27c104c',
   })
-  
+
   @IsUUID()
   reviewerId: string;
   @ApiProperty({
     description: 'The date by which the review is due',
     example: '2024-08-01T00:00:00.000Z',
   })
-  @IsNotEmpty()
-  @IsISO8601()
-  reviewDueDate: string;
+
+  
+  @IsDateString()
+  @IsOptional()
+  reviewDueDate?: Date;
 }
