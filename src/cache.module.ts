27b49a7631bd2324c8326e24f16bb2e26776e217
ibrahim muhammad <ibrex29@gmail.com>
2024-08-03 15:24:56 +0100

    fixed token

--- src/cache.module.ts
+++ src/cache.module.ts
@@ -1,36 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { CacheModule, CacheModuleAsyncOptions } from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-redis-store';
+import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
-    CacheModule.registerAsync({
+    CacheModule.register({
       isGlobal: true,
-      imports: [ConfigModule], // No need to call ConfigModule.forRoot again
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        const isActive = ['true', '1', 'yes'].includes(
-          configService.get<string>('API_REDIS_STORE_IS_ACTIVE'),
-        );
-        return {
-          store:
-            isActive &&
-            (await redisStore({
-              // Store-specific configuration:
-              socket: {
-                reconnectStrategy: (times) => {
-                  return Math.min(times * 50, 2000);
-                },
-                host: configService.get<string>('API_REDIS_HOST'),
-                port: +configService.get<number>('API_REDIS_PORT'),
-                passphrase: configService.get<string>('API_REDIS_PASS'),
-              },
-            })), // use redis when available or default to cache store
-          ttl: 5000, // milliseconds
-          max: 10, // maximum number of items in cache
-        } as CacheModuleAsyncOptions;
-      },
+      ttl: 5000, // milliseconds
+      max: 10, // maximum number of items in cache
     }),
   ],
 })
