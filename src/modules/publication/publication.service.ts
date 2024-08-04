f847db95094a44002e4ba8599a18f6418bba81fd
ibrahim muhammad <ibrex29@gmail.com>
2024-08-04 11:39:53 +0100

    pulications

--- src/modules/publication/publication.service.ts
+++ src/modules/publication/publication.service.ts
@@ -15,49 +15,49 @@ export class PublicationService {
         });
       }
 
-    async publishManuscript(publishManuscriptDto: PublishManuscriptDto, userId: string) {
-        const { manuscriptId, title, abstract, keywords,issue,volume,doi, formattedManuscript } = publishManuscriptDto;
+    // async publishManuscript(publishManuscriptDto: PublishManuscriptDto, userId: string) {
+    //     const { manuscriptId, title, abstract, keywords,issue,volume,doi, formattedManuscript } = publishManuscriptDto;
         
-        // Fetch the manuscript to check its status
-        const manuscript = await this.prisma.manuscript.findUnique({
-          where: { id: manuscriptId }
-        });
+    //     // Fetch the manuscript to check its status
+    //     const manuscript = await this.prisma.manuscript.findUnique({
+    //       where: { id: manuscriptId }
+    //     });
       
-        if (!manuscript) {
-          throw new BadRequestException('Manuscript not found');
-        }
+    //     if (!manuscript) {
+    //       throw new BadRequestException('Manuscript not found');
+    //     }
       
-        if (manuscript.status !== 'ACCEPTED') {
-          throw new BadRequestException('Manuscript status must be ACCEPTED By reviewer to be published');
-        }
+    //     if (manuscript.status !== 'ACCEPTED') {
+    //       throw new BadRequestException('Manuscript status must be ACCEPTED By reviewer to be published');
+    //     }
       
-        // Update manuscript status to published
-        const updatedManuscript = await this.prisma.manuscript.update({
-          where: { id: manuscriptId },
-          data: {
-            status: 'PUBLISHED',
-            isPublished: true,
-            updatedAt: new Date(),
-            updatedBy: userId
-          }
-        });
+    //     // Update manuscript status to published
+    //     const updatedManuscript = await this.prisma.manuscript.update({
+    //       where: { id: manuscriptId },
+    //       data: {
+    //         status: 'PUBLISHED',
+    //         isPublished: true,
+    //         updatedAt: new Date(),
+    //         updatedBy: userId
+    //       }
+    //     });
       
-        await this.prisma.publication.create({
-          data: {
-            title: title,
-            abstract: abstract,
-            keywords: keywords,
-            issue:issue,
-            volume:volume,
-            DOI:doi,
-            userId: userId,
-            formattedManuscript: formattedManuscript ,
-            manuscriptId: manuscriptId
-          }
-        });
+    //     await this.prisma.publication.create({
+    //       data: {
+    //         title: title,
+    //         abstract: abstract,
+    //         keywords: keywords,
+    //         issue:issue,
+    //         volume:volume,
+    //         DOI:doi,
+    //         userId: userId,
+    //         formattedManuscript: formattedManuscript ,
+    //         manuscriptId: manuscriptId
+    //       }
+    //     });
       
-        return updatedManuscript;
-      }
+    //     return updatedManuscript;
+    //   }
       
 
 async getAllPublishedManuscripts() {
